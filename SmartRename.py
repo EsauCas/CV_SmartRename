60b90c13bec1b0830a6f0718813e51ad1817305a
EsauCas <138743458+EsauCas@users.noreply.github.com>
2025-01-06 19:49:00 -0600

    Create SmartRename.py

---
+++ SmartRename.py
@@ -0,0 +1,11 @@
+import os
+
+carpeta = 'C:/Users/chane/Desktop/ProyectosCV/#1'
+lista_archivos = os.listdir(carpeta)
+texto_buscar = input("Introduce el texto a buscar: ")
+texto_reemplazar = input("Introduce el texto de reemplazo: ")
+
+for nombre_archivo in lista_archivos:
+    origen = os.path.join(carpeta, nombre_archivo)
+    destino = os.path.join(carpeta, nombre_archivo.replace(texto_buscar, texto_reemplazar))
+    os.rename(origen, destino)
